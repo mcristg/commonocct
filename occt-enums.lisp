bdeaba3250d5d6f9f774ed88ba7b4ee00b535d50
mcristg <mcristernag@gmail.com>
2024-07-26 01:29:17 -0700

    Fix Closed parenthesis

--- occt-enums.lisp
+++ occt-enums.lisp
@@ -9,29 +9,29 @@
 ;; Key modifier, for combining with general key from Aspect_VKey.
 (defconstant +Aspect_VKeyFlags_NONE+ 0)
 ;; reserve first 8 bits to combine value with Aspect_VKey
-(defconstant +Aspect_VKeyFlags_SHIFT+ (ash 1 8)  ; Aspect_VKey_Shift
-(defconstant +Aspect_VKeyFlags_CTRL+  (ash 1 9)  ; Aspect_VKey_Control
-(defconstant +Aspect_VKeyFlags_ALT+   (ash 1 10) ; Aspect_VKey_Alt
-(defconstant +Aspect_VKeyFlags_MENU+  (ash 1 11) ; Aspect_VKey_Menu
-(defconstant +Aspect_VKeyFlags_META+  (ash 1 12) ; Aspect_VKey_Meta
+(defconstant +Aspect_VKeyFlags_SHIFT+ (ash 1 8))  ; Aspect_VKey_Shift
+(defconstant +Aspect_VKeyFlags_CTRL+  (ash 1 9))  ; Aspect_VKey_Control
+(defconstant +Aspect_VKeyFlags_ALT+   (ash 1 10)) ; Aspect_VKey_Alt
+(defconstant +Aspect_VKeyFlags_MENU+  (ash 1 11)) ; Aspect_VKey_Menu
+(defconstant +Aspect_VKeyFlags_META+  (ash 1 12)) ; Aspect_VKey_Meta
 
 (defconstant +Aspect_VKeyFlags_ALL+ (logior +Aspect_VKeyFlags_SHIFT+ 
                                             +Aspect_VKeyFlags_CTRL+ 
                                             +Aspect_VKeyFlags_ALT+
                                             +Aspect_VKeyFlags_MENU+
-                                            +Aspect_VKeyFlags_META+)
+                                            +Aspect_VKeyFlags_META+))
  
 ;; Mouse buttons, for combining with Aspect_VKey and Aspect_VKeyFlags.                                            
 ;; Mouse button bitmask
 (defconstant +Aspect_VKeyMouse_NONE+ 0)       ; no buttons
 
-(defconstant +Aspect_VKeyMouse_LeftButton+   (ash 1 13) ; mouse left   button
-(defconstant +Aspect_VKeyMouse_MiddleButton+ (ash 1 14) ; mouse middle button (scroll)
-(defconstant +Aspect_VKeyMouse_RightButton+  (ash 1 15) ; mouse right  button
+(defconstant +Aspect_VKeyMouse_LeftButton+   (ash 1 13)) ; mouse left   button
+(defconstant +Aspect_VKeyMouse_MiddleButton+ (ash 1 14)) ; mouse middle button (scroll)
+(defconstant +Aspect_VKeyMouse_RightButton+  (ash 1 15)) ; mouse right  button
 
 (defconstant +Aspect_VKeyMouse_MainButtons+ (logior +Aspect_VKeyMouse_LeftButton+ 
                                                     +Aspect_VKeyMouse_MiddleButton+ 
-                                                    +Aspect_VKeyMouse_RightButton+)
+                                                    +Aspect_VKeyMouse_RightButton+))
 
 ;; BinLDrivers_Marker.hxx
 ;; enum BinLDrivers_Marker
